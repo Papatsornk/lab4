acf7a859582617d0e521652cd388878a52bf888c
Papatsornk <47176919+Papatsornk@users.noreply.github.com>
2019-02-14 17:14:51 +0700

    Add files via upload

---
+++ src/todo/TaskList.js
@@ -0,0 +1,17 @@
+
+import React,{Component} from 'react'
+
+class TaskList extends Component {
+   render() {
+       if ( this.props.tasks )
+           return (<ul > {
+                   this.props.tasks.map((item) => (
+                       <li key={item.id}> {item.task} {item.place} </li>
+                   ))
+               }
+           </ul>)
+   }
+}
+
+export default TaskList
+
